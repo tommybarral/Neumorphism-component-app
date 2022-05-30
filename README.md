ee4929781d4598f9c9dd1c82defe077627b33412
Tommybarral <isamutom27@hotmail.com>
2022-05-30 11:49:05 +0200

    UI with neumorphism

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Neumorphism Design Component app
 ## packages used / paquets utilisés
 none
 
-![]()
+![cap1.png](images/cap1.png)
+![cap2.png](images/cap2.png)
--- lib/screens/neumorphism_screen.dart
+++ lib/screens/neumorphism_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
 class NeumorphismScreen extends StatelessWidget {
-  const NeumorphismScreen({Key? key}) : super(key: key);
+  NeumorphismScreen({Key? key}) : super(key: key);
+
+  Widget neumorphismContainer(double height, double width, Widget? child) {
+    return  Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(color: Colors.grey.shade500, offset: const Offset(4, 4), blurRadius: 15, spreadRadius: 1),
+            const BoxShadow(color: Colors.white, offset: Offset(-4, -4), blurRadius: 15, spreadRadius: 1),
+          ]
+      ),
+      child: child,
+    );
+  }
+  
+  final List<IconData>? _interaction = [
+    Icons.arrow_back_ios,
+    Icons.pause,
+    Icons.arrow_forward_ios,
+  ];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -14,33 +38,57 @@ class NeumorphismScreen extends StatelessWidget {
               height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.1,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  Icon(Icons.menu, size: 32,),
-                  Text('Playlist', style: TextStyle(fontSize: 26, fontWeight: FontWeight.w400, letterSpacing: 1.5, color: Colors.black54),),
-                  Icon(Icons.more_horiz, size: 32,),
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.07,
+                    width: MediaQuery.of(context).size.width * 0.13,
+                    decoration: BoxDecoration(
+                      color: Colors.white54,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                      child: const Icon(Icons.menu, size: 32,),
+                  ),
+                  const Text('Playlist', style: TextStyle(fontSize: 26, fontWeight: FontWeight.w400, letterSpacing: 1.5, color: Colors.black54),),
+                  const Icon(Icons.more_horiz, size: 32,),
                 ],
               ),
             ),
-            SizedBox(
-              height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.66,
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: Column(
+            neumorphismContainer(
+            (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.64,
+            MediaQuery.of(context).size.width * 0.9,
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.network('https://ipurple.eu/1013-large_default/twice-more-more.jpg', fit: BoxFit.cover, height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.56),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.01,),
-                          const Text('More & More', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),),
-                          const Text('JYP Entertainment', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black54,),),
-                        ],
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.01,),
+                  Center(
+                    child: Container(
+                      height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.52,
+                      width: MediaQuery.of(context).size.width * 0.86,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        image: DecorationImage(
+                            image: NetworkImage('https://ipurple.eu/1013-large_default/twice-more-more.jpg'),
+                            fit: BoxFit.cover,
+                        ),
                       ),
-                      const Icon(Icons.favorite_border_outlined, color: Colors.red, size: 34,),
-                    ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5.0, left: 15.0, right: 15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.01,),
+                            const Text('More & More', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),),
+                            const Text('JYP Entertainment', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black54,),),
+                          ],
+                        ),
+                        const Icon(Icons.favorite_border_outlined, color: Colors.red, size: 34,),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -61,14 +109,9 @@ class NeumorphismScreen extends StatelessWidget {
                   ),
                   Stack(
                     children: [
-                      Container(
-                        height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.02,
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                      ),
+                      neumorphismContainer(
+                          (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.02,
+                          MediaQuery.of(context).size.width * 0.7, null),
                       Positioned(
                         top: 3,
                         left: 5,
@@ -87,13 +130,13 @@ class NeumorphismScreen extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.1,
+              height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 0.12,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  Icon(Icons.arrow_back_ios, color: Colors.black54,size: 32),
-                  Icon(Icons.pause, color: Colors.black54, size: 32),
-                  Icon(Icons.arrow_forward_ios, color: Colors.black54, size: 32),
+                children: [
+                  neumorphismContainer(70, 70, Icon(_interaction![0], color: Colors.black54,size: 32),),
+                  neumorphismContainer(70, 70, Icon(_interaction![1], color: Colors.black54, size: 32),),
+                  neumorphismContainer(70, 70, Icon(_interaction![2], color: Colors.black54, size: 32),),
                 ],
               ),
             ),
