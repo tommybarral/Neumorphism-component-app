a27bd3487b3ba086b296db7776309f005166800f
Tommybarral <isamutom27@hotmail.com>
2022-05-30 09:41:51 +0200

    starting component project

---
+++ android/app/src/main/kotlin/com/example/neumorphism_component/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.neumorphism_component
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:neumorphism_component/screens/neumorphism_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: NeumorphismScreen(),
+    );
+  }
+}
